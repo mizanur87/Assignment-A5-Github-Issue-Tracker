645a9947e15df247bdf4c471f53e0bb4a92821a1
mizanur87 <mirzarazmizan87@gmail.com>
2026-03-09 05:52:13 +0600

    search option added

--- home.js
+++ home.js
@@ -4,8 +4,49 @@ console.log("hello home.js");
 
 const dashContainer = document.querySelector("#dashContainer");
 const loadingSpin = document.querySelector("#loadingSpin");
+const filterBtn = document.querySelectorAll(".tg");
+const srchBox = document.querySelector("#srchBox");
+const srchBtn = document.querySelector("#srchBtn");
+let issues = "";
+let filtValue = "all";
+
+// Search Function
+
+srchBox.addEventListener("input", () => {
+  const val = srchBox.value.trim();
+  console.log(val);
+  searchItem(val);
+});
 
 // Show Loading Function
+
+filterBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    filterBtn.forEach((btn) => btn.classList.remove("active"));
+    btn.classList.add("active");
+    filtValue = btn.value;
+    console.log(btn.value);
+    filtrOut(filtValue);
+  });
+});
+
+function filtrOut(value) {
+  showloading();
+  let openCards = issues.filter((el) => el.status === "open");
+
+  let closedCards = issues.filter((el) => el.status === "closed");
+
+  if (value === "all") {
+    displayCards(issues);
+    hideloading();
+  } else if (value === "open") {
+    displayCards(openCards);
+    hideloading();
+  } else {
+    displayCards(closedCards);
+    hideloading();
+  }
+}
 
 function showloading() {
   loadingSpin.classList.remove("hidden");
@@ -24,20 +65,22 @@ async function loadAll(params) {
   hideloading();
 
   displayCards(data.data);
+  issues = data.data;
 }
 
 loadAll();
 
 function displayCards(cards) {
+  dashContainer.innerHTML = "";
   cards.forEach((element) => {
-    console.log(element);
+    // console.log(element);
     const card = document.createElement("div");
     card.className = "card bg-base-100 w-max-50 shadow-sm rounded-[4px]";
     card.innerHTML = `<div class="card-body px-4 space-y-1.5">
               <!-- top buttons -->
               <div class="flex justify-between">
                 <div class="flex justify-center items-center">
-                  <img src="./assets/Open-Status.png" alt="" />
+                  <img src = "${element.status === "open" ? "/assets/Open-Status.png" : "/assets/Closed- Status .png"}" />
                 </div>
 
                 <button
@@ -81,4 +124,23 @@ function displayCards(cards) {
             </div>`;
     dashContainer.appendChild(card);
   });
+}
+
+function searchItem(item) {
+  const query = item.toLowerCase();
+  let searched = [];
+  if (query.length > 0) {
+    for (let eleme of issues) {
+      if (
+        eleme.title.toLowerCase().includes(query) ||
+        eleme.author.toLowerCase().includes(query) ||
+        eleme.priority.toLowerCase().includes(query)
+      ) {
+        searched.push(eleme);
+      }
+    }
+    displayCards(searched);
+  } else {
+    displayCards(issues);
+  }
 }
